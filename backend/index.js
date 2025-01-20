1b21a2a6bd2856befd40a099c0bc0eeeb55802e3
Geryiman <geryiman0@gmail.com>
2025-01-19 18:46:55 -0600

    Buscador activo
    aqui el buscador ya funciona solo que nos manda todo en json, un texto en la panalla

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MeiliSearch } = require('meilisearch');
+const path = require('path');
 
 const app = express();
 const client = new MeiliSearch({
@@ -7,16 +8,24 @@ const client = new MeiliSearch({
   apiKey: '6d9bb59dca80659707c17d1363e5393fd13475362644c14faf0652ee886'
 });
 
-async function performSearch() {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/search', async (req, res) => {
+  const query = req.query.q;
+
   try {
-    const index = client.index('movies'); 
-    const search = await index.search(Search());
-    console.log('Resultados de búsqueda:', search);
+    const index = client.index('movies');
+    const searchResults = await index.search(query); 
+    res.json(searchResults);
   } catch (error) {
     console.error('Error al realizar la búsqueda:', error);
+    res.status(500).json({ error: 'Ocurrió un error al buscar las películas.' });
   }
-}
-performSearch();
+});
 app.listen(3000, () => {
   console.log('Servidor escuchando en el puerto 3000');
 });
