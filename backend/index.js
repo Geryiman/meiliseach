11a9a1b310f2951c39dda3207f5a4e47788dff7c
Geryiman <geryiman0@gmail.com>
2025-01-19 18:11:28 -0600

    Prebas de conexion con philadelphia
    Segun internet en el codigo para realizar la peticion lo hacemos primero con un consol para ver que haya conexion

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { MeiliSearch } = require('meilisearch');
+
+const app = express();
+const client = new MeiliSearch({
+  host: 'http://172.233.131.83:7700',
+  apiKey: '6d9bb59dca80659707c17d1363e5393fd13475362644c14faf0652ee886'
+});
+
+async function performSearch() {
+  try {
+    const index = client.index('movies'); 
+    const search = await index.search('philadelphia');
+    console.log('Resultados de búsqueda:', search);
+  } catch (error) {
+    console.error('Error al realizar la búsqueda:', error);
+  }
+}
+performSearch();
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
